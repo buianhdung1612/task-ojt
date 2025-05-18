4936fbe0df47c30dc0541c00fe3e80981d3fb9c3
Bui Anh Dung <buianh09dung@gmail.com>
2025-05-18 09:38:38 +0700

    ...

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors())
 
 const routeClient = require("./routes/client/index.route");
 routeClient(app);
