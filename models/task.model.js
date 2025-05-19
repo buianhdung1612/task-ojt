97694e466f1b966543e2606d5518a904f97c83d4
Bui Anh Dung <buianh09dung@gmail.com>
2025-05-19 18:49:59 +0700

    ...

--- models/task.model.js
+++ models/task.model.js
@@ -3,19 +3,21 @@ const mongoose = require('mongoose');
 const taskSchema = new mongoose.Schema(
     {
         title: String,
+        taskParentId: String,
         status: String, // "initial", "pending", "doing", "finish", "notFinish"
         content: String,
         timeStart: Date,
         timeFinish: Date,
+        createdBy: String,
+        listUser: Array,
         deleted: {
             type: Boolean,
             default: false
         },
         deletedAt: Date,
-        createdBy: String
     }, {
-        timestamps: true
-    }
+    timestamps: true
+}
 );
 
 const Task = mongoose.model('Task', taskSchema, "tasks");
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,6 @@ const userRoute = require("./users.route");
 const userMiddleware = require("../../middlewares/client/user.middleware");
 
 module.exports = (app) => {
-    app.use("/tasks", taskRoute);
+    app.use("/tasks", userMiddleware.requireAuth, taskRoute);
     app.use("/users", userRoute);
 }   
